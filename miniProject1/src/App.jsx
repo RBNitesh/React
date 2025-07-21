c7a6a1f4235a86b55b16e3f896b51392ec2b6abc
Nitesh kumar <nk41521391@gmail.com>
2025-07-21 10:31:25 +0530

    new file added

--- miniProject1/src/App.jsx
+++ miniProject1/src/App.jsx
@@ -1,14 +1,58 @@
 import "./App.css";
 import "tailwindcss";
 function App() {
+  function handleClick(event) {
+    const btn = event.target;
+    // const color = btn.innerText.toLowerCase();
+    const color = btn.style.background;
+    document.body.style.background = color;
+    console.log(`Backgrounnd color changed to ${color}`);
+  }
+
   return (
     <div>
-      <button className="btn">Red</button>
-      <button className="btn">Blue</button>
-      <button className="btn">Black</button>
-      <button className="btn">Green</button>
-      <button className="btn">Gray</button>
-      <button className="btn">Yellow</button>
+      <button
+        className="btn"
+        style={{ background: "red" }}
+        onClick={handleClick}
+      >
+        Red
+      </button>
+      <button
+        className="btn"
+        style={{ background: "blue" }}
+        onClick={handleClick}
+      >
+        Blue
+      </button>
+      <button
+        className="btn"
+        style={{ background: "black" }}
+        onClick={handleClick}
+      >
+        Black
+      </button>
+      <button
+        className="btn"
+        style={{ background: "green" }}
+        onClick={handleClick}
+      >
+        Green
+      </button>
+      <button
+        className="btn"
+        style={{ background: "gray" }}
+        onClick={handleClick}
+      >
+        Gray
+      </button>
+      <button
+        className="btn"
+        style={{ background: "yellow" }}
+        onClick={handleClick}
+      >
+        Yellow
+      </button>
     </div>
   );
 }
