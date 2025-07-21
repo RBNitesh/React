1286708f18ddb3eeb2c3826087200f667a0dcdec
Nitesh kumar <nk41521391@gmail.com>
2025-07-21 19:55:16 +0530

    new file

--- miniProject2/src/App.jsx
+++ miniProject2/src/App.jsx
@@ -1,35 +1,88 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import "./App.css";
 
 function App() {
   const [length, setLength] = useState(8);
-  const [numberAllowed, setNumnberAllowed] = useState(true);
-  const [charAllowed, setCharAllowed] = useState(true);
-  const [Password, setPassword] = useState("");
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
-  const passwordGenerator = useCallback(() => {
-    let pass = "";
-    let str = "ABCDEFGHIJKLMNOPWRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  //method to generate password
+  const generatePassword = useCallback(() => {
+    let password = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    if (numberAllowed) str += "0123456789";
-    if (charAllowed) str += "!@#$%^*-_={}~`";
-
-    for (let i = 1; i <= length; i++) {
-      let char = Math.floor(Math.random() * str.length + 1);
-      pass += str.charAt(char);
+    if (numberAllowed) {
+      str += "0123456789";
     }
 
-    setPassword(pass);
+    if (charAllowed) {
+      str += "!@#$%^&*()_+[]{}|;:,.<>?";
+    }
+
+    for (let i = 0; i < length; i++) {
+      let idx = Math.floor(Math.random() * str.length + 1);
+
+      password += str.charAt(idx);
+    }
+    //set the generated password
+    setPassword(password);
   }, [length, numberAllowed, charAllowed]);
 
+  //useEffect to call generatePassword when dependencies change
+  useEffect(() => {
+    generatePassword();
+  }, [length, numberAllowed, charAllowed, generatePassword]);
+
   return (
-    <>
-      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 my-8 text-orange-500 bg-gray-600">
-        <h1 className="text-4xl text-center text- text-white p-4">
-          Password Generator
-        </h1>
+    <div className="box">
+      <h1>Password generator</h1>
+      <div>
+        <input
+          className="pass-box"
+          type="text"
+          value={password}
+          name="password"
+          placeholder="enter password"
+          readOnly
+        />
+        <button className="copy-btn">copy</button>
       </div>
-    </>
+      <div className="innerBox">
+        <input
+          type="range"
+          className="cursor-pointer"
+          onChange={(event) => setLength(event.target.value)}
+          min={6}
+          max={100}
+          value={length}
+          id="length-range"
+        />
+        <label htmlFor="length-range">Length: {length}</label>
+
+        <input
+          type="checkbox"
+          onChange={() => {
+            setNumberAllowed((prev) => !prev);
+          }}
+          checked={numberAllowed}
+          className="number-allowed"
+          id="number-allowed"
+        />
+        <label htmlFor="number-allowed">numbers</label>
+
+        <input
+          type="checkbox"
+          onChange={() => {
+            setCharAllowed((prev) => !prev);
+          }}
+          checked={charAllowed}
+          className="char-allowed"
+          id="char-allowed"
+        />
+        <label htmlFor="char-allowed">characters</label>
+      </div>
+    </div>
   );
 }
 
