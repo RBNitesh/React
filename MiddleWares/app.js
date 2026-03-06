48065cc7294e218ba28231ead342ac7d39ce637c
Nitesh Kumar <nk41521391@gmail.com>
2026-03-06 23:11:51 +0530

    custome error handler class

--- MiddleWares/app.js
+++ MiddleWares/app.js
@@ -1,18 +1,41 @@
 const express = require("express");
+const ExpressError = require("./ExpressError");
 const app = express();
-
-app.get("/err", (req, res) => {
-  err = err;
-});
 
 app.get("/", (req, res) => {
   res.send("running...");
 });
 
+const checkToken = (req, res, next) => {
+  let { q } = req.query;
+  if (q === "giveaccess") {
+    next();
+  }
+  // custom error class
+  throw new ExpressError(401, "ACCESS DENIED!"); // First Error Handler
+};
+
+app.get("/api", checkToken, (req, res) => {
+  res.send("data");
+});
+
+// Handling error using custoem error handler class
+app.get("/admin", (request, response) => {
+  throw new ExpressError(403, "Access to admin is forbidden!");
+});
+
+app.get("/err", (req, res) => {
+  err = err;
+});
+
 app.use((err, req, res, next) => {
+  // Second Error Handler
   console.log("Error is detected using custom error handling middleware");
-  res.status(500).json({ message: "Internal Server Error" });
-  //   next(err); // this passes the error to default handler which print the stack
+  res
+    .status(err.status || 500)
+    .json({ message: err.message, status: err.status || 500 });
+  // next(err); // this passes the error to default handler which print the stack
+  // next(); // calling non error handling middleware
 });
 
 // starting the server
