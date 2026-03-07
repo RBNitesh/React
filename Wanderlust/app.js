235588722ef996d6a741bc4baa9ef98ab1e81960
Nitesh Kumar <nk41521391@gmail.com>
2026-03-07 21:18:19 +0530

    Handling invalid input

--- Wanderlust/app.js
+++ Wanderlust/app.js
@@ -130,7 +130,8 @@ app.use((req, res, next) => {
 // custom error handler
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).send(message);
+  // res.status(statusCode).send(message);
+  res.status(statusCode).render("listings/error.ejs", { message });
 });
 
 // starting the server
