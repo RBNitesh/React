8cf2f424a09dbf5cc8ce8538be08a8777f5ed029
Nitesh Kumar <nk41521391@gmail.com>
2026-03-09 23:33:46 +0530

    Relationship in mongodb

--- RelationsShips/Models/user.js
+++ RelationsShips/Models/user.js
@@ -10,33 +10,93 @@ async function main() {
 }
 
 // One to few relationship implementation
-const userSchema = new Schema({
-  username: String,
-  addresses: [
-    {
-      _id: false,
-      location: String,
-      city: String,
-    },
-  ],
+// const userSchema = new Schema({
+//   username: String,
+//   addresses: [
+//     {
+//       _id: false,
+//       location: String,
+//       city: String,
+//     },
+//   ],
+// });
+
+// const User = mongoose.model("User", userSchema);
+
+// const addUser = async () => {
+//   let user1 = new User({
+//     username: "maxtern",
+//     addresses: [
+//       {
+//         location: "abc street",
+//         city: "Navi Mumbai",
+//       },
+//     ],
+//   });
+//   user1.addresses.push({
+//     location: "Lal Singh Chadha chowk",
+//     city: "Amritsahar",
+//   });
+
+//   let result = await user1.save();
+//   console.log(result);
+// };
+
+// addUser();
+
+// Implementation of One to Many RelationShips
+const orderSchema = new Schema({
+  item: String,
+  price: Number,
 });
 
-const User = mongoose.model("User", userSchema);
+// model
+const Order = mongoose.model("Order", orderSchema);
 
-const addUser = async () => {
-  let user1 = new User({
-    username: "sherlock",
-    addresses: [
-      {
-        location: "221B Baker Street",
-        city: "London",
-      },
-    ],
-  });
-  user1.addresses.push({ location: "Chandani Chowk", city: "Delhi" });
+// const addOrder = async () => {
+//   let res = await Order.insertMany([
+//     { item: "Samosa", price: 10 },
+//     { item: "Tea", price: 10 },
+//     { item: "Cake", price: 800 },
+//   ]);
+//   console.log(res);
+// };
 
-  let result = await user1.save();
-  console.log(result);
+// addOrder();
+
+const customerSchema = new Schema({
+  name: String,
+  // Got this type for ObjectId from mongoose populate section
+  orders: [{ type: Schema.Types.ObjectId, ref: "Story" }],
+});
+
+const Customer = mongoose.model("Customer", customerSchema);
+
+const addCustomer = async () => {
+  let order1 = await Order.findOne({ item: "Samosa" });
+  let order2 = await Order.findOne({ item: "Tea" });
+  let order3 = await Order.findOne({ item: "Cake" });
+
+  let customer1 = {
+    name: "Zyan",
+    orders: [order1, order2, order3],
+  };
+
+  let res = await Customer.insertOne(customer1);
+  console.log(res);
 };
 
-addUser();
+const addCustomer = async () => {
+  let order1 = await Order.findOne({ item: "Samosa" });
+  let order2 = await Order.findOne({ item: "Tea" });
+
+  let customer2 = new Customer({
+    name: "Jiva",
+    orders: [order1, order2],
+  });
+
+  let res = await customer2.save();
+  console.log(res);
+};
+
+// addCustomer();
