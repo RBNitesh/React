9677984dde870e6ce7a1af354120ad30e34e23b7
Nitesh Kumar <nk41521391@gmail.com>
2026-03-15 23:17:00 +0530

    Express router is implemented to simplify the routing

--- Wanderlust/routes/listing.js
+++ Wanderlust/routes/listing.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({
+  caseSensitive: true,
+  mergeParams: true,
+  strict: true,
+});
 
 const methodOverride = require("method-override");
 const Listing = require("../models/listing");
